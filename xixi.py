6c0307ff07b7c7848e746476f57fe0f5976d225c
lingtengqiu <1259738366@qq.com>
2022-07-04 12:32:17 +0800

    python-snippets

--- xixi.py
+++ xixi.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-7 -*-
+"""
+@File: xixi.py
+@Author: Lingteng Qiu
+@Email: qiulingteng@link.cuhk.edu.cn
+@Date: 2022-07-02
+@Desc:
+"""
 
+def get_parse():
+    parser = argparse.ArgumentParser(description='id')
+    parser.add_argument('--mode', default='train', help='train or test', choices=['train', 'test', 'profile'])
+    parser.add_argument('--device', default='cuda', help='select model')
+    parser.add_argument('dd', default='dd', help='dd')
+
+
+    args = parser.parse_args()
+    return args
+def get_parse():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--mode', default='train', help='train or test', choices=['train', 'test', 'profile'])
+    parser.add_argument('--device', default='cuda', help='select model')
+    parser.add_argument('--', default='', help='')
+    parser.add_argument('--', default='', help='')
+
+    args = parser.parse_args()
+    return args
 import math
 import numpy as np
 import cv2
